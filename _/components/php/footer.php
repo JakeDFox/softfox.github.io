7569d33e9d4c62364cef1edc83ab63184cfcd8df
Jake <cinema.jdf@gmail.com>
2018-04-20 21:06:07 -0700

    moving-from-web-building

---
+++ _/components/php/footer.php
@@ -0,0 +1,10 @@
+<section class="container">
+	<footer class="row">
+		<nav class="col-lg-12">
+			<ul class="breadcrumb">
+				<li><a href="about.php">About the Roux Academy</a></li>
+				<li><a href="privacypolicy.php">Privacy Policy</a></li>
+				<li><a href="http://rouxacademy.com">Roux Academy Website</a></li>
+			</ul>
+	</footer>
+</section>
